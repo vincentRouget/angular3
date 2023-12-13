6bec6090c5dfb3934c92945f1a1b2dbe75f24f76
vincentRouget <vincent.rouget86@gmail.com>
2023-12-13 21:54:45 +0100

    Finished

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  user = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+  citation: string = "";
+  hide: boolean = true;
+  hideAge(): void {
+    this.hide = !this.hide;
+  }
+}
